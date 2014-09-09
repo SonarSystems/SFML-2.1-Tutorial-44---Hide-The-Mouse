1b58bf4fc83c8112b3601a3f95df1b2373404b6e
Frahaan Hussain <Sonar Systems>
2014-09-09 09:22:20 +0100

    Hide The Mouse

---
+++ TutorialSFML/TutorialSFML/main.cpp
@@ -0,0 +1,30 @@
+#include "SFML/Graphics.hpp"
+#include <iostream>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(600, 600), "SFML WORK!");
+
+	window.setMouseCursorVisible(false);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+
+				break;
+
+			}
+		}
+
+		window.clear();
+
+		window.display();
+	}
+}
